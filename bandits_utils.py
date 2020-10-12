af96d196059876acbef5040b23c29e226e6857ca
Aymeric JAN <aymeric.jan@gmail.com>
2020-10-12 18:00:06 +0200

    modified:   bandits_utils.py

--- bandits_utils.py
+++ bandits_utils.py
@@ -3,6 +3,95 @@ import pandas as pd
 from scipy.sparse.linalg import svds
 from sklearn.decomposition import NMF
 
+def genere_X_t(i, means, std):
+    """
+    Génère une réalisation X_t issue de la loi N(means[i], std[i])
+
+    input:
+        i (int):
+            Indice du bras
+
+    output:
+        float:
+            X_t ~ N(means[i], std[i])
+    """
+    ## La distribution doit être 1 sub-ussian
+    return np.random.normal(means[i],std[i])
+
+def UCB_i(i, delta, mu, T):
+    """
+    Renvoie la valeur de la fonction UBC_i, eq 7.2, page 102
+
+    inputs:
+        i (int):
+            Numéro du bras
+        delta (float):
+            Valeur de delta (eq 7.1)
+        mu (1 x k NumpyArray):
+            Vecteur de la moyenne empirique de chacun des bras
+        T (1 x k NumpyArray):
+            Vecteur contenant le nombre de fois où chaque bras a été tiré
+    
+    output:
+        float:
+            Valeur de la UCB pour le bras i
+
+    """
+    if T[i] == 0:
+        return np.inf
+    else:
+        return mu[i] + np.sqrt(2 * np.log(1/delta)/T[i])
+
+def UCB_delta(n, delta, k, means, std):
+
+    """
+    Implémentation de l'algorithme UCB(delta), Algo 3 page 102
+
+    inputs:
+        n (int):
+            Nombre d'itérations
+        delta (float):
+            Valeur de delta (eq 7.1)
+        k (int):
+            Nombre de bras
+        means (1 x k NumpyArray):
+            Vecteur des moyennes
+        std (1 x k NumpyArray):
+            Vecteur des variances
+    
+    output:
+        X (1 x n NumpyArray):
+            Réalisations (X_t)_t
+        Rn (1 x n NumpyArray):
+            Regrets (R_t)_t
+        A (1 x n NumpyArray):
+            Bras choisis
+        mu (1 x k NumpyArray):
+            Moyennes empiriques de chaque bras    
+
+    """
+    X = np.zeros(n) ##Réalisations (X_t)_t
+    T = np.zeros(k) ##T[i] = nb de fois où i a été tiré
+    arms_mat = np.zeros((n,k))
+    mu = np.zeros(k) ##mu[i] = moyenne empirique de i
+
+    A = np.zeros(n, dtype = int)
+    
+    ##Compute Rn at each iteration
+    Rn = np.zeros(n)
+    mu_star = np.max(means)
+    for t in range(n):
+        all_A_t = np.zeros(k)
+        for i in range(k):
+            all_A_t[i] = UCB_i(i, delta, mu, T) ##On calcul UCB_i pour tous les bras
+        A[t] = np.argmax(all_A_t) ##On prend le bras qui a le UCB_i le plus haut
+        X[t] = genere_X_t(A[t], means, std) ##On génère X_t la réalisation à partir du bras choisi
+        T[A[t]] += 1 ##Le bras i a été tiré une fois de plus
+        arms_mat[t, A[t]] = X[t] ##On stock la valeur de la réalisation dans une matrice
+        mu[A[t]] = 1/T[A[t]] * sum(arms_mat[:, A[t]]) ##On update la moyenne empirique
+        Rn[t] = (t+1)*mu_star - T@means ##On calcul le regret à l'instant t
+    return X, Rn, A, mu
+
 def non_neg_decomp(M, d):
     """
     Renvoie le vecteur H de la décomposition NMF telle que M = WH
@@ -18,7 +107,7 @@ def non_neg_decomp(M, d):
             Résultat de la "Non-negative matrix factorization"
     """
     model = NMF(n_components=d, init='random', random_state=0)
-    W = model.fit_transform(M)
+    model.fit_transform(M)
     H = model.components_
     return H
 
@@ -79,91 +168,3 @@ def LinUCB(T, user, H, d):
 
     return a_t, history, regret
 
-def UCB(i, delta, mu, T):
-    """
-    Renvoie la valeur de la fonction UBC_i, eq 7.2, page 102
-
-    inputs:
-        i (int):
-            Numéro du bras
-        delta (float):
-            Valeur de delta (eq 7.1)
-        mu (1 x k NumpyArray):
-            Vecteur de la moyenne empirique de chacun des bras
-        T (1 x k NumpyArray):
-            Vecteur contenant le nombre de fois où chaque bras a été tiré
-    
-    output:
-        float:
-            Valeur de la UCB pour le bras i
-
-    """
-    if T[i] == 0:
-        return np.inf
-    else:
-        return mu[i] + np.sqrt(2 * np.log(1/delta)/T[i])
-
-def UCB_tot(n, delta, k, means, std):
-
-    """
-    Implémentation de l'algorithme UCB(delta), Algo 3 page 102
-
-    inputs:
-        n (int):
-            Nombre d'itérations
-        delta (float):
-            Valeur de delta (eq 7.1)
-        k (int):
-            Nombre de bras
-        means (1 x k NumpyArray):
-            Vecteur des moyennes
-        std (1 x k NumpyArray):
-            Vecteur des variances
-    
-    output:
-        X (1 x n NumpyArray):
-            Réalisations (X_t)_t
-        Rn (1 x n NumpyArray):
-            Regrets (R_t)_t
-        A (1 x n NumpyArray):
-            Bras choisis
-        mu (1 x k NumpyArray):
-            Moyennes empiriques de chaque bras    
-
-    """
-    X = np.zeros(n) ##Réalisations (X_t)_t
-    T = np.zeros(k) ##T[i] = nb de fois où i a été tiré
-    arms_mat = np.zeros((n,k))
-    mu = np.zeros(k) ##mu[i] = moyenne empirique de i
-
-    A = np.zeros(n, dtype = int)
-    
-    ##Compute Rn at each iteration
-    Rn = np.zeros(n)
-    mu_star = np.max(means)
-    for t in range(n):
-        all_A_t = np.zeros(k)
-        for i in range(k):
-            all_A_t[i] = UCB(i, delta, mu, T) ##On calcul UCB_i pour tous les bras
-        A[t] = np.argmax(all_A_t) ##On prend le bras qui a le UCB_i le plus haut
-        X[t] = launch_arm(A[t]) ##On génère X_t la réalisation à partir du bras choisi
-        T[A[t]] += 1 ##Le bras i a été tiré une fois de plus
-        arms_mat[t, A[t]] = X[t] ##On stock la valeur de la réalisation dans une matrice
-        mu[A[t]] = 1/T[A[t]] * sum(arms_mat[:, A[t]]) ##On update la moyenne empirique
-        Rn[t] = (t+1)*mu_star - T@means ##On calcul le regret à l'instant t
-    return X, Rn, A, mu
-
-def launch_arm(i):
-    """
-    Lance le bras i
-
-    input:
-        i (int):
-            Indice du bras
-
-    output:
-        float:
-            X_t ~ N(means[i], std[i])
-    """
-    ## La distribution doit être 1 sub-ussian
-    return np.random.normal(means[i],std[i])
